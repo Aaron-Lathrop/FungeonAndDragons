845a1d8176a184ce9e0cdc8b888fa1627654abdc
Aaron Lathrop <alathrop78@gmail.com>
2020-05-16 08:05:39 -0600

    add animated movile nav menu

--- src/components/MobileMenu.vue
+++ src/components/MobileMenu.vue
@@ -1,50 +1,54 @@
 <template>
-	<div class="menu-container" onclick="myFunction(this)">
-		<div class="bar1"></div>
-		<div class="bar2"></div>
-		<div class="bar3"></div>
-	</div>
+  <div class="menu-container">
+    <div class="bar1"></div>
+    <div class="bar2"></div>
+    <div class="bar3"></div>
+  </div>
 </template>
 
 <script>
 export default {
-	name: "MobileMenu",
+  name: "MobileMenu"
 };
 </script>
 
 <style lang="less" scoped>
+.menu-container {
+  cursor: pointer;
+}
+
 .bar1,
 .bar2,
 .bar3 {
-	width: 25px;
-	height: 2px;
-	background-color: #fff;
-	margin: 6px 0;
-	transition: 0.4s;
+  width: 25px;
+  height: 2px;
+  background-color: #fff;
+  margin: 6px 0;
+  transition: 0.4s;
 }
 
 /* Rotate first bar */
 .change .bar1 {
-	-webkit-transform: rotate(-45deg) translate(-9px, 6px);
-	transform: rotate(-45deg) translate(-9px, 6px);
+  -webkit-transform: rotate(-45deg) translate(-9px, 6px);
+  transform: rotate(-45deg) translate(-9px, 6px);
 }
 
 /* Fade out the second bar */
 .change .bar2 {
-	opacity: 0;
+  opacity: 0;
 }
 
 /* Rotate last bar */
 .change .bar3 {
-	-webkit-transform: rotate(45deg) translate(-8px, -8px);
-	transform: rotate(45deg) translate(-8px, -8px);
+  -webkit-transform: rotate(45deg) translate(-8px, -8px);
+  transform: rotate(45deg) translate(-8px, -8px);
 }
 
 @media only screen and (min-width: 600px) {
-	.bar1,
-	.bar2,
-	.bar3 {
-		display: none;
-	}
+  .bar1,
+  .bar2,
+  .bar3 {
+    display: none;
+  }
 }
 </style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,78 +1,123 @@
 <template>
-	<nav class="nav">
-		<ul class="container">
-			<li class="nav-logo">Fungeon & Dragons</li>
-			<li class="nav-link">Adventures</li>
-			<li class="nav-link">Characters</li>
-			<li class="nav-link">About</li>
-			<mobile-menu />
-		</ul>
-	</nav>
+  <nav class="nav">
+    <ul class="container">
+      <li class="nav-logo">Fungeon & Dragons</li>
+      <li class="nav-link-container slidein" :class="{ hide: hideNavLinks }">
+        <a class="nav-link">Adventures</a>
+        <a class="nav-link">Characters</a>
+        <a class="nav-link">About</a>
+      </li>
+      <li @click="toggleNavLinks()">
+        <mobile-menu />
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <script>
 import MobileMenu from "./MobileMenu.vue";
 export default {
-	name: "NavBar",
-	components: {
-		MobileMenu,
-	},
+  name: "NavBar",
+  components: {
+    MobileMenu
+  },
+  data: () => {
+    return {
+      hideNavLinks: true
+    };
+  },
+  methods: {
+    toggleNavLinks() {
+      setTimeout(() => (this.hideNavLinks = !this.hideNavLinks), 200);
+    }
+  }
 };
 </script>
 
 <style lang="less" scoped>
+@keyframes slidein {
+  0% {
+    right: -200px;
+  }
+  100% {
+    right: 0;
+  }
+}
+
+.slidein {
+  animation: 200ms ease-in-out slidein;
+}
+
+.nav-link-container {
+  position: absolute;
+  top: 60px;
+  right: 0;
+  padding: 25px 40px;
+  background-color: #090809;
+
+  & a {
+    display: block;
+  }
+}
+
 .nav {
-	margin: auto;
-	padding: 15px;
-	color: #cbf2ca;
-	background-color: #090809;
-	box-shadow: 0px 2px 11px #ccc;
+  margin: auto;
+  padding: 15px;
+  color: #cbf2ca;
+  background-color: #090809;
+  box-shadow: 0px 2px 6px #ccc;
+  max-width: 100vw;
+  position: relative;
 
-	& ul {
-		display: flex;
-		display: -ms-flexbox;
-		align-items: center;
-		list-style-type: none;
-		font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  & ul {
+    display: flex;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    align-items: center;
+    list-style-type: none;
+    font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
 
-		& .nav-link {
-			display: none;
-		}
-
-		& .nav-logo {
-			display: -ms-flexbox;
-			display: -webkit-flex;
-			display: flex;
-			flex: 1;
-			font-size: 20px;
-			font-weight: bold;
-			margin-right: 55px;
-			/* min-width: 236.141px; */
-		}
-
-		& .nav-link {
-			margin-right: 25px;
-		}
-	}
+    & .nav-logo {
+      display: -ms-flexbox;
+      display: -webkit-flex;
+      display: flex;
+      flex: 1 auto;
+      font-size: 16px;
+      font-weight: bold;
+      margin-right: 55px;
+      /* min-width: 236.141px; */
+    }
+  }
 }
 
 @media only screen and (min-width: 600px) {
-	.nav {
-		& ul {
-			display: flex;
-			align-items: center;
-			list-style-type: none;
-			font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  .nav-link-container {
+    flex: 0 auto;
+    display: flex;
+    position: static;
+    padding: 0;
+  }
+  .nav {
+    & ul {
+      display: flex;
+      align-items: center;
+      list-style-type: none;
+      font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
 
-			& .nav-link {
-				display: inline-flex;
-			}
+      & .nav-link {
+        display: inline-flex;
+        margin-right: 25px;
+      }
 
-			& .nav-logo::before {
-				content: url(/assets/favicon/favicon-32x32.png);
-				margin-right: 15px;
-			}
-		}
-	}
+      & .nav-logo {
+        font-size: 20px;
+      }
+
+      & .nav-logo::before {
+        content: url(/assets/favicon/favicon-32x32.png);
+        margin-right: 15px;
+      }
+    }
+  }
 }
 </style>
